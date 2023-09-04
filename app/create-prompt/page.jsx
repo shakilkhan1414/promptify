0858fbed312e4573394bbbd3339d712e140ad767
Shakil Khan <khan.shakil.1414@gmail.com>
2023-09-05 02:28:41 +0600

    initialize project

---
+++ app/create-prompt/page.jsx
@@ -0,0 +1,24 @@
+'use client'
+
+import { useState } from "react"
+import { useSession } from "next-auth/react"
+import { useRouter } from "next/navigation"
+import Form from "@/components/Form"
+
+const page = () => {
+    const [submitting,setSubmitting]=useState(false)
+    const [post,setPost]=useState({
+        prompt:'',
+        tag: '',
+    })
+
+    const createPrompt=()=>{
+
+    }
+
+  return (
+    <Form type="Create" post={post} setPost={setPost} submitting={submitting} handleSubmit={createPrompt} />
+  )
+}
+
+export default page
